52d1bf48b6ae283f208af88b189c5fd27c8f416b
GiancarloG <giancarlogravagnatrabajo@gmail.com>
2023-03-14 15:58:16 -0300

    Segundo Guardado

---
+++ Objetos.js
@@ -0,0 +1,44 @@
+
+//A diferencia de las matrices que tienen elementos valorados en índices, los objetos usan un concepto llamado pares de clave:valor. 
+
+//La clave (key) es el identificador y el valor (value) es el valor que queremos guardar en esa clave. La sintaxis es "clave: valor". Los objetos pueden contener muchos pares de clave-valor, deben estar separados por una coma (importante: sin punto y coma dentro de un objeto). 
+
+//Las claves son únicas en un objeto, solo puede haber una clave de ese nombre, aunque, varias claves pueden tener el mismo valor. Los valores pueden ser cualquier tipo de dato de Javascript; cadena, número, booleano, matriz, función o incluso otro objeto. 
+
+
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+};
+
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+ //Para acceder a la propiedad de un objeto simplemente tenemos que escribir el nombre del objeto seguido de un punto y el nombre de la propiedad.
+
+//Para cambiar el valor de una propiedad simplemente tenemos que acceder a ella e igualarla al nuevo valor.
+
+//Para eliminar propiedades utilizaremos una palabra reservada llamada delete.
+
+
+// ACCEDER
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+// ASIGNAR
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
