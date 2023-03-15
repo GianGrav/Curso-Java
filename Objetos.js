b696a6e9eda10965c12e318c71470cd4c038b069
GiancarloG <giancarlogravagnatrabajo@gmail.com>
2023-03-15 10:00:15 -0300

    Tercer Avance

--- Objetos.js
+++ Objetos.js
@@ -42,3 +42,56 @@ console.log(autos);
 //BORRAR
 delete autos.marcas;
 console.log(autos);
+
+//De la misma manera que utilizamos la Dot-Notation o notación por puntos para acceder o asignar un valor, también podemos usar Bracket-Notation, o notación por corchetes. Lo único que cambia es la forma en la que lo escribimos.
+
+//Muchas veces nos puede suceder que necesitemos utilizar una variable externa para guardarla como propiedad en un objeto. Es importante que en esos casos recordemos utilizar Bracket-Notation sin comillas para que funcione correctamente.
+
+
+// DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+
+
+//Recorridos en objetos
+
+//De la misma manera que podíamos recorrer los elementos de un arreglo, podemos recorrer las propiedades de un objeto junto con sus valores.
+//El bucle For-In nos permite iterar sobre un objeto utilizando dos variables pivot. Una representa el objeto que recorremos, y la otra la propiedad en la que se está realizando la iteración. Utilizando ambos valores podremos acceder al valor de cada propiedad del objeto.
+
+
+//Contextos en JavaScript
+
+//Dentro de este lenguaje de programación existe un objeto global llamado this. Este nos permite manipular el contexto en el que las funciones y la información está siendo ejecutada. De esta forma podremos tener un alcance más preciso dentro de nuestro código.
+
+
+
+// HAS OWN PROPERTY
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+for (let prop in mundo) {
+   console.log('Esta es la propiedad: ', prop);
+   console.log('Este es el valor: ', mundo[prop]);
+}
+
+//THIS
+var mascota = {
+   animal: 'Perro',
+   raza: 'Ovejero Alemán',
+   amistoso: true,
+   dueño: 'María López',
+   info: () => {
+      console.log('Mi perro es un  ' + this.raza);
+   },
+};
